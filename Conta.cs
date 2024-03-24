731e12b7720da9e73a06e18133b5a257e24367d7
João Vitor <joaobrambati@gmail.com>
2024-03-24 19:11:27 -0300

    Adicionar arquivos de projeto.

---
+++ Conta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1 {
+    internal class Conta {
+
+        public int Numero {  get; private set; }
+        public string Nome {  get; private set; }
+        public double Saldo {  get; private set; }
+        public string Chave { get; private set; }
+        public DateTime Comprovante { get; private set; }
+
+        public Conta(int numero, string nome, string chave, DateTime comprovante) {
+            Numero = numero;
+            Nome = nome;
+            Chave = chave;
+            Comprovante = comprovante;
+        }
+        public Conta(int numero, string nome, string chave, DateTime comprovante, double depositoInicial) 
+        :this(numero, nome, chave, comprovante) {
+            Saldo = depositoInicial;
+        }
+
+        public void Deposito(double deposito) {
+            Saldo += deposito;
+        }
+        public void Saque(double saque) {
+            Saldo -= saque+5.0;
+        }
+
+        public void Guid(string chave) {
+            Chave = chave;
+        }
+        public void Data(DateTime comprovante) {
+            Comprovante = comprovante;
+        }
+
+        public override string ToString() {
+            return "Número: "
+                   + Numero
+                   + "\nTitular: "
+                   + Nome
+                   + "\nSaldo: R$ "
+                   + Saldo.ToString("F2", CultureInfo.InvariantCulture)
+                   + "\nChave: "
+                   + Chave
+                   + "\n"
+                   + Comprovante;                   
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        static void Main (string[] args) {
+
+            Conta conta;
+
+            Console.WriteLine("***** Santander *****");
+                string chave = Guid.NewGuid().ToString().Substring(0, 8);
+                var comprovante = DateTime.Now;
+            Console.Write("Número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Nome do titular: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine().ToLower());
+                if (resposta == 's') {
+                    Console.Write("Valor do depósito inicial: ");
+                    double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    conta = new Conta(numero, nome, chave, comprovante, depositoInicial);
+                }
+                else {
+                    conta = new Conta(numero, nome, chave, comprovante);
+                }
+
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.WriteLine($"--- Dados da Conta ---\n{conta}");
+
+            Console.WriteLine();
+            Console.Write("Valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            chave = Guid.NewGuid().ToString().Substring(0, 8);
+            conta.Guid(chave);
+            comprovante = DateTime.Now;
+            conta.Data(comprovante);
+
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.WriteLine($"--- Dados da Conta ---\n{conta}");
+            
+
+            Console.WriteLine();
+            Console.Write("Valor do saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            chave = Guid.NewGuid().ToString().Substring(0, 8);
+            conta.Guid(chave);
+            comprovante = DateTime.Now;
+            conta.Data(comprovante);
+
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.WriteLine($"--- Dados da Conta ---\n{conta}");
+
+        }
+    }
+}
